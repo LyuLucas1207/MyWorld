04a056816200fc3ba03c40cf8a8a3b6f5473abd6
Lyu <lyuchongkai@gmail.com>
2024-09-22 22:30:32 -0800

    rest

--- src/js/admin_home.js
+++ src/js/admin_home.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import '../css/index.css';
 // import reportWebVitals from './templates/reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
+// import BrowserRouter from 'react-router-dom';
 import BrowserRouter from '../routers/admin_home_router';
 
 const root = ReactDOM.createRoot(document.getElementById('admin_home-root'));
